0e0468b2de070e5050ab5006093059b056a21774
Wong Jia Hau <hou32hou@gmail.com>
2020-06-06 15:49:42 +0800

    Update

--- sample.test.ts
+++ sample.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "./src/deps.ts"
-export { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
+import { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
 
 Deno.test('sample', () => {
   assertEquals(1, 1)
