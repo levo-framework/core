bea760e90d5c62cda3ff34d65e3b8a7cc8cfc4be
Wong Jia Hau <hou32hou@gmail.com>
2020-06-06 15:26:31 +0800

    Update main.yml

--- sample.test.ts
+++ sample.test.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
+export * as babelstandalone from "https://dev.jspm.io/@babel/standalone@7.10.1";
 
 Deno.test('sample', () => {
   assertEquals(1, 1)
