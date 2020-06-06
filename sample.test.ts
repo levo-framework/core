2a448106ea01ea58a3d30903373a56b40e90d64e
Wong Jia Hau <hou32hou@gmail.com>
2020-06-06 16:33:15 +0800

    Revert unecessary changes

--- sample.test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals } from "./src/deps.ts"
-import { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
-
-Deno.test('sample', () => {
-  assertEquals(1, 1)
-  brotliCompress(brotliCompress(new TextEncoder().encode('hello')))
-})
