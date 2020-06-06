354dc872e992a858408f5209428ba1f403382db4
Wong Jia Hau <hou32hou@gmail.com>
2020-06-06 15:34:47 +0800

    Update

--- sample.test.ts
+++ sample.test.ts
@@ -1,16 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
-export * as babelstandalone from "https://dev.jspm.io/@babel/standalone@7.10.1";
-
-export * as server from "https://deno.land/std@0.53.0/http/server.ts";
-
-export * as path from "https://deno.land/std@0.53.0/path/mod.ts";
-
-export {
-  gzipDecode,
-  gzipEncode,
-} from "https://github.com/manyuanrong/wasm_gzip/raw/53d036/mod.ts";
-
-// export { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
+import { assertEquals } from "./src/deps.ts"
 
 Deno.test('sample', () => {
   assertEquals(1, 1)
