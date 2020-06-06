a05dab6f949505a2fb361531abefa06e4b83b60c
Wong Jia Hau <hou32hou@gmail.com>
2020-06-06 15:30:20 +0800

    Update

--- sample.test.ts
+++ sample.test.ts
@@ -10,7 +10,7 @@ export * as path from "https://deno.land/std@0.53.0/path/mod.ts";
 //   gzipEncode,
 // } from "https://github.com/manyuanrong/wasm_gzip/raw/53d036/mod.ts";
 
-// export { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
+export { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
 
 Deno.test('sample', () => {
   assertEquals(1, 1)
