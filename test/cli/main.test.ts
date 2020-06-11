1e255a4a830ad0351fc7b8c9dbedacc76914aae4
Wong Jia Hau <hou32hou@gmail.com>
2020-06-11 23:49:21 +0800

    Improve levo cli test

--- test/cli/main.test.ts
+++ test/cli/main.test.ts
@@ -2,17 +2,38 @@ import { getDirectoryTree } from "../../src/get-directory-tree.ts";
 import { assertEquals } from "../../src/deps.ts";
 
 Deno.test({
-  name: "new-project command",
+  name: "new-project and new-page command",
   fn: async () => {
     const projectName = "hello";
-    await Deno.run({
-      cmd:
-        `deno run --allow-all --unstable cli/mod.ts new-project ${projectName}`
-          .split(" "),
-      stdout: "piped",
-      stderr: "piped",
-    })
-      .output();
+    const runCommand = (command: string) => {
+      const process = Deno.run({
+        cmd: command.split(" "),
+        stdout: "piped",
+        stderr: "piped",
+      });
+      const decoder = new TextDecoder();
+      return Promise.all([process.output(), process.stderrOutput()])
+        .then(([output, err]) => {
+          if (err.length > 0) {
+            return decoder.decode(err);
+          } else {
+            return decoder.decode(output).trim();
+          }
+        })
+        .then((result) => {
+          console.log(result);
+          return result;
+        });
+    };
+
+    console.log("\nInstalling Levo CLI");
+    await runCommand(
+      `deno install --allow-all --unstable --force --name levo cli/mod.ts`,
+    );
+
+    console.log("Creating new project using Levo CLI");
+    await runCommand(`levo new-project ${projectName}`);
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
     const tree = await getDirectoryTree(
       `./${projectName}`,
@@ -73,28 +94,36 @@ Deno.test({
       ["tsconfig.json"],
     ]);
 
-    console.log(Deno.cwd())
-
-    // Test if the server created with the templates work
-    Deno.chdir(projectName)
-
+    console.log('Test if the server created with the templates work')
+    Deno.chdir(projectName);
     const server = new Worker(`./${projectName}/app.ts`, {
       type: "module",
       //@ts-ignore
       deno: true,
     });
 
-    await new Promise(resolve => setTimeout(resolve, 3000))
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    const response = await fetch('http://localhost:5000')
-    assertEquals(response.status, 200)
-    assertEquals(response.headers.get('content-type'), 'text/html')
+    const response = await fetch("http://localhost:5000");
+    assertEquals(response.status, 200);
+    assertEquals(response.headers.get("content-type"), "text/html");
 
-    
-    
-    // Teardown
-    server.terminate()
-    Deno.chdir('..')
+    // Test new-page command
+    // await runCommand(
+    //   `deno run --allow-all --unstable cli/mod.ts new-project ${projectName}`,
+    // );
+
+    console.log("Tear down");
+    console.log(`Uninstall Levo CLI`);
+    const levoPath = await runCommand(`which levo`);
+    console.log("levoPath", levoPath);
+    await Deno.remove(levoPath);
+
+    console.log("Terminate server");
+    server.terminate();
+
+    console.log("Delete created project folder");
+    Deno.chdir("..");
     await Deno.remove(projectName, { recursive: true });
   },
   sanitizeOps: false,
