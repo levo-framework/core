359fb30e43db504097e74e72e877904114f2d078
Wong Jia Hau <hou32hou@gmail.com>
2020-06-14 22:48:45 +0800

    Fix #35

--- demo/home/view.ts
+++ demo/home/view.ts
@@ -11,11 +11,30 @@ export const view = (model: Model): Levo.Element<Action> => {
         ["link", { rel: "stylesheet", href: "./levo.assets/index.css" }],
       ]],
       model.currentValue.toString(),
-      ["button", { onclick: $.add() }, ["add"]],
+      [
+        "button",
+        {
+          onclick: $.add(),
+          style: {
+            backgroundColor: model.currentValue % 2 === 0
+              ? "red"
+              : "green",
+          },
+        },
+        ["add"],
+      ],
       ["button", { onclick: $.minus() }, ["minus"]],
       ["button", { onclick: $.stop_interval() }, ["stop timer"]],
       ["button", { onclick: $.fetch() }, ["fetch"]],
       `Text: ${model.text}`,
+      [
+        "input",
+        {
+          type: "checkbox",
+          checked: model.currentValue % 2 === 0 ? "true" : undefined,
+        },
+      ],
+      ["input", { value: model.currentValue % 2 === 0 ? "even" : "odd" }],
       [
         "body",
         {},
