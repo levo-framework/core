ae9f01ee76bd37145a3128e67d5f0bdb31763d07
Wong Jia Hau <hou32hou@gmail.com>
2020-06-20 10:27:59 +0800

    Updat

--- hello.ts
+++ hello.ts
@@ -1,4 +1,4 @@
-import { brotliCompress } from "./src/deps.ts"
+import { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
 
 Deno.test('hello', () => {
   brotliCompress(new TextEncoder().encode('his'))
