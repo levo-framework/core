f585b4eaeebd0287bbda9762f7154d0e4f1ed233
Wong Jia Hau <hou32hou@gmail.com>
2020-06-21 19:33:19 +0800

    Update github workflow

--- hello.ts
+++
@@ -1,6 +0,0 @@
-import { compress as brotliCompress } from "https://deno.land/x/brotli@v0.1.3/mod.ts";
-
-Deno.test('hello', () => {
-  brotliCompress(new TextEncoder().encode('his'))
-  console.log('helo')
-})
