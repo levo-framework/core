0fd45c51c6f1827060bc8fdc6b3f820f14488c79
Wong Jia Hau <hou32hou@gmail.com>
2020-06-21 19:43:12 +0800

    Reset app.ts

--- demo/app.ts
+++ demo/app.ts
@@ -3,7 +3,7 @@ import { LevoApp } from "../mod/levo-app.ts";
 LevoApp.start({
   port: 3000,
   hostname: "0.0.0.0",
-  minifyJs: true,
+  minifyJs: false,
   cachePages: false,
   rootDir: new URL("./home", import.meta.url),
 });
