74540a197fb5dc16e8e46fc65515326bbb3084db
Wong Jia Hau <hou32hou@gmail.com>
2020-06-22 17:49:40 +0800

    Update

--- tools/verify-levo-runtime-raw.sh
+++ tools/verify-levo-runtime-raw.sh
@@ -1,3 +1,4 @@
 git status | grep "levo-runtime-raw.ts" \
-&& echo "levo-runtime-raw.ts is not up to date" 1>&2 \
-&& exit 1
+  && echo "levo-runtime-raw.ts is not up to date" 1>&2 \
+  && exit 1
+exit 0
