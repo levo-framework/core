ca0ad9654e104ff9797f80aebbfa9f635b51d5ac
WJH <hou32hou@gmail.com>
2020-06-27 15:27:40 +0800

    Setting up test for levo-runtime (#64)

--- tools/format.sh
+++ tools/format.sh
@@ -1,7 +1,8 @@
-deno fmt demo
-deno fmt mod
-deno fmt src
-deno fmt test
-deno fmt templates
-deno fmt cli
-deno fmt tools
+deno fmt $1 demo
+deno fmt $1 mod
+deno fmt $1 src
+deno fmt $1 test
+deno fmt $1 templates
+deno fmt $1 cli
+deno fmt $1 tools
+deno fmt $1 ./test-levo-runtime/test.js
