ed328b56cb85d377fee9cd61f92982cbc55e4f93
Wong Jia Hau <hou32hou@gmail.com>
2020-07-14 16:03:52 +0800

    Self vendor Babel standalone to improve server compile time

--- src/deps.ts
+++ src/deps.ts
@@ -13,8 +13,6 @@ export * as server from "https://deno.land/std@0.59.0/http/mod.ts";
 
 export * as path from "https://deno.land/std@0.61.0/path/mod.ts";
 
-export * as babelstandalone from "https://dev.jspm.io/@babel/standalone@7.10.1";
-
 import _CleanCSS from "https://dev.jspm.io/clean-css@4.2.3";
 const CleanCSS: {
   new (): {
