467c0da7ee28ab24a0ff75986a21ea3393aff158
Wong Jia Hau <hou32hou@gmail.com>
2020-07-15 11:01:34 +0800

    feat(template): Park environment under import map Fixes #99

--- templates/new-project/root/_server.ts
+++ templates/new-project/root/_server.ts
@@ -1,8 +1,9 @@
 import { view } from "./view.tsx";
 import { Model, Action, initialModel } from "./types.ts";
 import { serve } from "levo/levo-serve.ts";
+import { Environment } from "environment";
 
-export default serve<Model, Action, unknown>({
+export default serve<Model, Action, Environment>({
   getResponse: async (request, response) => {
     return response.page({
       view,
