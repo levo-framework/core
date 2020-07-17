f767a986890939e1a2ac1305dc2bcbe0f8b5c77a
Wong Jia Hau <hou32hou@gmail.com>
2020-07-18 01:01:07 +0800

    Update

--- test-levo-runtime/test.js
+++ test-levo-runtime/test.js
@@ -35,7 +35,7 @@ beforeAll(async (done) => {
   await waitForSeconds(30);
   browser = await puppeteer.launch({ headless: true });
   done();
-}, 30000);
+}, 40000);
 
 describe("", () => {
   test("case 1", async () => {
