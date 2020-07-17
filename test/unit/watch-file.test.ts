b03fa0b34dc5b680e304edd91e61385e4f07ddbb
Wong Jia Hau <hou32hou@gmail.com>
2020-07-17 23:32:55 +0800

    Update

--- test/unit/watch-file.test.ts
+++ test/unit/watch-file.test.ts
@@ -4,7 +4,7 @@ import { assertEquals } from "../deps.ts";
 Deno.test({
   name: "watch file",
   fn: async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     let counter = 0;
     const path = "./test/unit/test-files/x.ts";
@@ -15,14 +15,14 @@ Deno.test({
       },
     });
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const initialContent = await Deno.readFile(path);
 
     // Change the file x.ts
     await Deno.writeFile(path, new TextEncoder().encode("hello"));
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     // Expect onChange handlers is olny triggered once
     assertEquals(counter, 1);
@@ -32,6 +32,6 @@ Deno.test({
 
     await handler.stop();
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   },
 });
