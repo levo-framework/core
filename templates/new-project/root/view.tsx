ff47c2466ecffce60848921431ffc95d46086a70
Wong Jia Hau <hou32hou@gmail.com>
2020-07-25 18:04:14 +0800

    feat(template): include viewport meta tag

--- templates/new-project/root/view.tsx
+++ templates/new-project/root/view.tsx
@@ -9,6 +9,7 @@ export const view = (
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="title" content="My Levo Page" />
         <meta name="description" content="This is a Levo page." />
 
